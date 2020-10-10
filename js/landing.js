b77b41c1c346abed122aeaf29d20f0d2945c115d
vaish1601 <54760363+vaish1601@users.noreply.github.com>
2020-10-11 01:00:27 +0400

    Add files via upload

---
+++ js/landing.js
@@ -0,0 +1,13 @@
+var content = 'If life is so bleh, and meh then maybe use our resources smile. . .';
+
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+
+$(ele).hide().appendTo('p').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
